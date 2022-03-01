1f7c156ea4c7e7f9ee7f2a101e9061a8569f5a77
Rasmus Kibshede <2740ralle@gmail.com>
2022-03-01 15:14:07 +0100

    Version 2

--- js/script.js
+++ js/script.js
@@ -1,33 +1,59 @@
 const url = "http://worldtimeapi.org/api/timezone/";
-const dropdown = document.querySelector("#dropdown");
+const dropdown_Continent = document.querySelector("#dropdown_Continent");
+const dropdown_City = document.querySelector("#dropdown_City");
 const lblTime = document.querySelector("#time");
-
 
 const lblCity = document.querySelector("#timezone-city");
 const lblContinent = document.querySelector("#timezone-continent");
 const lblUTC = document.querySelector("#UTC");
 
+
 fillDropdown(url);
 
 function fillDropdown(url) {
+  let listOfContinents = [];
   fetch(url)
     .then(res => res.json())
     .then((res) => {
       for (let i = 0; i < res.length; i++) {
+
+        const locationName = res[i].split("/");
         if (res[i].includes('/') && !res[i].includes("Etc")) {
-          const dropdownOptions = document.createElement("option");
-
-          const locationName = res[i].split("/");
-
-          dropdownOptions.textContent = locationName[0] + " (" + locationName[1] + ")";
-          dropdownOptions.value = res[i];
-          dropdown.appendChild(dropdownOptions);
+          if (!listOfContinents.includes(locationName[0])) {
+            listOfContinents = locationName[0];
+            fillDropdownContinent(locationName);
+          }
         }
       }
     });
 }
 
+function fillDropdownContinent(locationName) {
+  const dropdownOption = document.createElement("option");
+  dropdownOption.textContent = locationName[0];
+  dropdownOption.value = locationName[0];
+  dropdown_Continent.appendChild(dropdownOption);
+}
 
+
+function fillDropdownCity() {
+  console.log(url + dropdown_Continent.value);
+  fetch(url + dropdown_Continent.value)
+    .then(con => con.json())
+    .then(con => {
+
+      dropdown_City.textContent = "";
+      for (let i = 0; i < con.length; i++) {
+        const locationName = con[i].split("/");
+        const dropdownOption = document.createElement("option");
+
+        dropdownOption.textContent = locationName[1];
+        dropdownOption.value = locationName[1];
+        dropdown_City.appendChild(dropdownOption);
+      }
+    });
+
+}
 
 
 async function updateTime(url) {
@@ -47,30 +73,8 @@ function worldtimeapiFetch(url) {
       lblContinent.textContent = locationName[0];
       lblCity.textContent = locationName[1];
       lblUTC.textContent = "" + res.utc_offset;
-
-
-      /*{
-  "abbreviation": "CET",
-  "client_ip": "87.59.14.119",
-  "datetime": "2022-02-17T21:01:50.952225+01:00", <----
-  "day_of_week": 4,
-  "day_of_year": 48,
-  "dst": false,
-  "dst_from": null,
-  "dst_offset": 0,
-  "dst_until": null,
-  "raw_offset": 3600,
-  "timezone": "Europe/Copenhagen", <----
-  "unixtime": 1645128110,
-  "utc_datetime": "2022-02-17T20:01:50.952225+00:00",
-  "utc_offset": "+01:00", <----
-  "week_number": 7
-        }*/
-
-      /*TODO: lav passende attributter ud fra de markerede JSON værdier som kan manipulerer med klokken  */
-      /* Altså vælg tidszone fra drop down, og set uret.*/
     });
 }
 
-
-dropdown.addEventListener("change", () => updateTime(url + dropdown.value));
+dropdown_Continent.addEventListener('change', fillDropdownCity);
+dropdown_City.addEventListener("change", () => updateTime(url + (dropdown_Continent.value + "/" + dropdown_City.value)));
