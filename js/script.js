8056de786868c3e64801bf19a4b499e6dc3b80ef
Rasmus Kibshede <2740ralle@gmail.com>
2022-03-02 23:13:34 +0100

    Version 2.1 cleanup

--- js/script.js
+++ js/script.js
@@ -7,75 +7,56 @@ const lblCity = document.querySelector("#timezone-city");
 const lblContinent = document.querySelector("#timezone-continent");
 const lblUTC = document.querySelector("#UTC");
 
-fillDropdown();
-
 //combine fillDropdown func
-
 function fillDropdown() {
   dropdown_City.style.display = "none";
   let listOfContinents = [];
 
-  //remake the loop (res.foreach) and so on
   fetch(url)
     .then(res => res.json())
-    .then(res => {
-      for (let i = 0; i < res.length; i++) {
-        const locationName = res[i].split("/");
-        if (res[i].includes('/') && !res[i].includes("Etc")) {
-          if (!listOfContinents.includes(locationName[0])) {
-            listOfContinents = locationName[0];
-            fillDropdownContinent(locationName);
-          }
+    .then(res => res.forEach(data => {
+      const locationName = data.split("/");
+      if (data.includes('/') && !data.includes("Etc")) {
+        if (!listOfContinents.includes(locationName[0])) {
+          listOfContinents = locationName[0];
+
+          const dropdownOption = document.createElement("option");
+          dropdownOption.textContent = locationName[0];
+          dropdownOption.value = locationName[0];
+          dropdown_Continent.appendChild(dropdownOption);
         }
       }
-    });
+    }));
 }
-
-function fillDropdownContinent(locationName) {
-  const dropdownOption = document.createElement("option");
-  dropdownOption.textContent = locationName[0];
-  dropdownOption.value = locationName[0];
-  dropdown_Continent.appendChild(dropdownOption);
-}
-
 
 function fillDropdownCity() {
   dropdown_City.style.display = "inline-block";
-
-  //remake the loop (con.foreach) and so on
+  dropdown_City.textContent = "";
   fetch(url + dropdown_Continent.value)
-    .then(con => con.json())
-    .then(con => {
-      dropdown_City.textContent = "";
+    .then(res => res.json())
+    .then(res => res.forEach(data => {
 
-      for (let i = 0; i < con.length; i++) {
-        const locationName = con[i].split("/");
-        const dropdownOption = document.createElement("option");
-        const locationNameLength = locationName.length - 1;
+      const locationName = data.split("/");
+      const dropdownOption = document.createElement("option");
+      const locationNameLength = locationName.length - 1;
 
-        //remake so that is dynamic and not static
-        if (locationName.length === 3) {
-          dropdownOption.textContent = locationName[1] + " (" + locationName[locationNameLength] + ")";
-          dropdownOption.value = locationName[1] + "/" + locationName[locationNameLength];
-        } else {
-          dropdownOption.textContent = locationName[locationNameLength];
-          dropdownOption.value = locationName[locationNameLength];
-        }
-
-        dropdown_City.appendChild(dropdownOption);
+      //remake so that is dynamic and not static
+      if (locationName.length === 3) {
+        dropdownOption.textContent = locationName[1] + " (" + locationName[locationNameLength] + ")";
+        dropdownOption.value = locationName[1] + "/" + locationName[locationNameLength];
+      } else {
+        dropdownOption.textContent = locationName[locationNameLength];
+        dropdownOption.value = locationName[locationNameLength];
       }
-    });
-}
 
-function updateTime() {
-  setTimeout(updateTime, 1000);
-  worldtimeapiFetch()
+      dropdown_City.appendChild(dropdownOption);
+    }));
 }
 
 function worldtimeapiFetch() {
   fetch(url + (dropdown_Continent.value + "/" + dropdown_City.value))
-    .then((res) => res.json())
-    .then((res) => {
+    .then(res => res.json())
+    .then(res => {
       const locationName = res.timezone.split("/");
 
       lblTime.textContent = res.datetime.substring(11, 19);
@@ -83,7 +64,10 @@ function worldtimeapiFetch() {
       lblCity.textContent = locationName[1];
       lblUTC.textContent = "" + res.utc_offset;
     });
+  setTimeout(worldtimeapiFetch, 1000);
 }
 
+fillDropdown();
+
 dropdown_Continent.addEventListener('change', fillDropdownCity);
-dropdown_City.addEventListener("change", () => updateTime());
+dropdown_City.addEventListener("change", worldtimeapiFetch);
